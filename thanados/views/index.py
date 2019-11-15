2fd3ac7a961692f7606fb6c10dfde75934bb23f7
Stefan Eichert <stefan.eichert@univie.ac.at>
2019-11-15 19:29:50 +0100

    cleanup

--- thanados/views/index.py
+++ thanados/views/index.py
@@ -2,10 +2,7 @@ from flask import render_template
 
 from thanados import app
 
-projectname=app.config["PROJECT_NAME"]
-sitename="/index/index" + projectname + ".html"
-
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template(sitename)
+    return render_template("/index/index.html")
