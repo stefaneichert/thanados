f5fc72cf13a7b64d0aa70ef2459523b67137a57a
filzwieser <roland.filzwieser@gmail.com>
2020-04-21 22:05:21 +0200

    test first changes

--- thanados/static/js/manual.js
+++ thanados/static/js/manual.js
@@ -2,6 +2,9 @@ $(document).ready(function () {
     maximumHeight = ($(window).height() - $('#mynavbar').height());
     $('#mycontent').css('max-height', (maximumHeight) + 'px');
     $('.wrapper').css('height', (maximumHeight) + 'px');
+    $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
 });
 
 $(window).resize(function () {
