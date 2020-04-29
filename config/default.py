0b2c7f04710ec2016fb47e580bdbf4c23aa0142a
stefaneichert <stefan.eichert@univie.ac.at>
2020-04-29 13:15:51 +0200

    new variables in config

--- config/default.py
+++ config/default.py
@@ -10,3 +10,5 @@ SECRET_KEY = 'CHANGE ME'
 SITE_LIST = 0
 WEB_FOLDER_PATH = '/static/images/entities'
 UPLOAD_FOLDER_PATH = os.path.dirname(__file__) + '/../thanados' + WEB_FOLDER_PATH
+THUNDERFOREST_API_KEY = 'yourAPIKey'
+OPENATLAS_URL = 'https://url_to_database_backend + /entity/'
