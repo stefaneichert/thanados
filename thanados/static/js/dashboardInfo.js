9d07126ddc640ac94f830a07e147bf3caf25b831
NinaBrundke <nina.brundke@gmx.at>
2020-11-17 14:43:22 +0100

    changed remaining description for dashboardinfo

--- thanados/static/js/dashboardInfo.js
+++ thanados/static/js/dashboardInfo.js
@@ -1,19 +1,19 @@
 var DashboardInfo = [
     {
         "chart": "gravetypes",
-        "title": "Types of graves/features",
+        "title": "Types of Graves/Features",
         "description": "This pie chart visualises different types of graves and other features, found at the respective site, in relation to their frequency.",
         "data": ""
     },
     {
         "chart": "burialtypes",
-        "title": "Types of burials/stratigraphic units",
+        "title": "Types of Burials/Stratigraphic Units",
         "description": "This pie chart visualises different types of burials and other stratigraphic units, found at the respective site, in relation to their frequency.",
         "data": ""
     },
     {
         "chart": "gravegoods",
-        "title": "Burials with/without grave goods",
+        "title": "Burials with/without Grave Goods",
         "description": "This pie chart visualises burials with and without grave goods, found at the respective site, in relation to their frequency.",
         "data": ""
     },
@@ -22,5 +22,65 @@ var DashboardInfo = [
         "title": "title goes here",
         "description": "Here goes the description of the chart",
         "data": ""
-    }
+    },
+    {
+        "chart": "gravedepth",
+        "title": "Depth of Graves (cm)",
+        "description": "This bar chart visualises the depth of graves found at the respective site. The depth is given in cm and grouped into brackets.",
+        "data": ""
+    },
+    {
+        "chart": "gravelength",
+        "title": "Length of Graves (cm)",
+        "description": "This bar chart visualises the length of graves found at the respective site. The length is given in cm and grouped into brackets.",
+        "data": ""
+    },
+    {
+        "chart": "gravewidth",
+        "title": "Width of Graves (cm)",
+        "description": "This bar chart visualises the width of graves found at the respective site. The width is given in cm and grouped into brackets.",
+        "data": ""
+    },
+    {
+        "chart": "graveconstr",
+        "title": "Grave Construction",
+        "description": "This pie chart visualises the different types of grave construction found at the respective site, found at the respective site, in relation to their frequency.",
+        "data": ""
+    },
+    {
+        "chart": "gravedeg",
+        "title": "Orientation of Graves/Skeletons (0-360°)",
+        "description": "This bar chart visualises the orientation of the graves respectively the skeletons found at the site according to the publicated data. The orientation is given in degrees (from 0° to 360°) and grouped into brackets.",
+        "data": ""
+    },
+    {
+        "chart": "graveazi",
+        "title": "Azimuth of Grave Pits (0-180°)",
+        "description": "This bar chart visualises the azimuth of the graves found at the site. The azimuth is calculated from spatial information if no orientation is available in the publication(s). The azimuth is given in degrees (from 0° to 180°) and grouped into brackets.",
+        "data": ""
+    },
+    {
+        "chart": "burialage",
+        "title": "Age at Death based on Age Bracket Classification",
+        "description": "This bar chart visualises the age of death of the individuals buried at the respective site, based on defined age brackets. The chart is based on the following age brackets: Fetus: 0 to 10 lunar months, Neonate: birth to 3 months, Infans I: 3 months to 6 years, Infans II: 7 to 13 years, Juvenile: 14 to 19 years, Early Adult: 20 to 25 years, Adult: 20 to 39 years, Adult/Mature: 30 to 49 years, Mature: 40 to 59 years, Mature/Senil: 50 t0 79 years, Senil: 60+ years, Subadult: 0 to 19 years, Grown Up: 20+ years. The age is given in years and for each burial individually, sorted from young to old. Furthermore, an average age for the burial ground is calculated from the available data and displayed.",
+        "data": ""
+    },
+   {
+        "chart": "valueage",
+        "title": "Age at Death based on Absolute Age (min.-max.)",
+        "description": "This bar chart visualises the age of death of the individuals buried at the respective site, based on a given minimal and maximal age. The age is given in years and for each burial individually, sorted from young to old. Furthermore, an average age for the burial ground is calculated from the available data and displayed.",
+        "data": ""
+    },
+     {
+        "chart": "sex",
+        "title": "Anthropological Sex Estimation",
+        "description": "This pie chart visualises the biological sex distribution on the respective site. The estimation is based on anthropological methods. Possible categories are: Female, likely female, indifferent, likely male, male, undetermined, and no information. A sex is only given for grown-ups. Children are included into the category undetermined.",
+        "data": ""
+    },
+    {
+        "chart": "sexdepth",
+        "title": "Depth of Graves by Sex of Individuals",
+        "description": "This bar chart visualises the depth of the grave pit depending on the sex of the buried individual in the respective grave. The chart distinguishes between the sex categories male, female, and subadult. The depth of the grave pit is given in cm and and grouped into brackets.",
+        "data": ""
+    },
 ]
